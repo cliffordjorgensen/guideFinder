c912014ad8ccaddb757c8100641addb1d57e616b
Badri Vasistha <badri.vasistha@gmail.com>
2019-07-08 13:36:07 -0700

    traveler database added to schema.sql

--- app/data/schema.sql
+++ app/data/schema.sql
@@ -1,4 +1,4 @@
-DROP DATABASE guidefinder_db;
+DROP DATABASE if exists guidefinder_db;
 CREATE DATABASE guidefinder_db;
 
 USE guidefinder_db;
@@ -6,31 +6,15 @@ USE guidefinder_db;
 CREATE TABLE traveler(
 TravelerID INT AUTO_INCREMENT,
 Name VARCHAR (255) NULL,
-Age INT NULL,
 Location VARCHAR (100) NULL,
+Preference VARCHAR (255) NULL,
 PRIMARY KEY (TravelerID));
 
-INSERT INTO traveler (Name, Age) VALUES ('Kevin Malone', 45);
-INSERT INTO traveler (Name, Age) VALUES ('Creed Bratton', 55);
-INSERT INTO traveler (Name, Age) VALUES ('Oscar Martinez', 32);
-INSERT INTO traveler (Name, Age) VALUES ('Angela Martin', 23);
-INSERT INTO traveler (Name, Age) VALUES ('Toby Flenderson', 45);
-
-CREATE TABLE guide(
-GuideID INT AUTO_INCREMENT,
-Name VARCHAR (255) NULL,
-Age INT NULL,
-Location VARCHAR (100) NULL,
-Experience INT NULL,
-PRIMARY KEY (GuideID));
-
-INSERT INTO guide (Name, Age, Experience) VALUES ('Michael Scott', 42, 11);
-INSERT INTO guide (Name, Age, Experience) VALUES ('Jim Halpert', 34, 8);
-INSERT INTO guide (Name, Age, Experience) VALUES ('Stanley Hudson', 49, 11);
-INSERT INTO guide (Name, Age, Experience) VALUES ('Phyllis Smith', 42, 9);
-INSERT INTO guide (Name, Age, Experience) VALUES ('Dwight Scrhute', 35, 10);
-INSERT INTO guide (Name, Age, Experience) VALUES ('Pam Beesley', 29, 8);
+INSERT INTO traveler (Name, Preference) VALUES ('Kevin Malone', 'Cycling');
+INSERT INTO traveler (Name, Preference) VALUES ('Creed Bratton', 'Bus Tours');
+INSERT INTO traveler (Name, Preference) VALUES ('Oscar Martinez', 'Hiking');
+INSERT INTO traveler (Name, Preference) VALUES ('Angela Martin', 'Sight Seeing');
+INSERT INTO traveler (Name, Preference) VALUES ('Toby Flenderson', 'Beaches and Oceans');
+INSERT INTO traveler (Name, Preference) VALUES ('Kelly Kapoor', 'Surfing');
 
 SELECT * FROM traveler;
-
-SELECT * FROM guide;
