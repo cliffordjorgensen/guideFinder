5234ea79d3eacf669deba432cbd611deed921aaa
joswald123 <48632836+joswald123@users.noreply.github.com>
2019-07-08 14:05:13 -0700

    guideprofiles data base Api

---
+++ app/data/guides.js
@@ -0,0 +1,10 @@
+const guideProfiles = [
+    {
+        "name": "Lucas Brennan",
+        "photo": "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+        "activity name": "Tour in bus",
+        "home town": "San Francisco",
+        "years of experience": 10,
+        "description": "San Francisco Love Tour: On this San Francisco tour, I will drive you down the very steep and crooked Lombard Street, and around neighborhoods such as the Mission, the Castro, and North Beach without worrying about getting around or parking. we will venture down streets other bigger bus tours can’t go, and get in the loving spirit the City by the Bay is well known for as you spot famous musicians’ homes while listening to classic songs."
+    }
+]
