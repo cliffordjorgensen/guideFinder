171cde9bde64625555d59c724e250bf377a89b99
joswald123 <48632836+joswald123@users.noreply.github.com>
2019-07-09 14:40:42 -0700

    Fix repo

--- data/sql/schema.sql
+++ data/sql/schema.sql
@@ -1,20 +1,23 @@
-DROP DATABASE if exists guidefinder_db;
-CREATE DATABASE guidefinder_db;
-
-USE guidefinder_db;
-
 DROP DATABASE IF EXISTS guidefinder_db;
 CREATE DATABASE guidefinder_db;
 
 USE guidefinder_db;
-CREATE TABLE guideInfo(
-guide_id INT AUTO_INCREMENT,
+CREATE TABLE guideinfo(
+guideID INT AUTO_INCREMENT,
 name VARCHAR (255) NULL,
-photo_link VARCHAR (255) NULL,
+photolink VARCHAR(255) NULL,
 age INT NULL,
 activity VARCHAR(500) NULL,
 city VARCHAR(255) NULL,
+<<<<<<< Updated upstream
 years_experience INT NULL,
 activity_description VARCHAR(9001) NULL,
 PRIMARY KEY (guide_id)
 );
+=======
+yearsofExperience INT NULL,
+descriptionActivity VARCHAR(9001) NULL,
+PRIMARY KEY (guideID)); 
+
+SELECT * from guideinfo;
+>>>>>>> Stashed changes
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-var express = require("express");
-var exphbs = require("express-handlebars");
-var PORT = process.env.PORT || 3000;
-
-var app = express();
-
-// Serve static content for the app from the "public" directory in the application directory.
+const express = require("express");
+const exphbs = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
+const app = express();
 app.use(express.static("public"));
 
-// Parse request body as JSON
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-// var routes = require("./controllers/mainController.js");
+const mysql = require("mysql");
 
-// app.use(routes);
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "guidefinder_db"
+});
 
 var icecreams = [
     { name: 'vanilla', photolink: 10, age: 3, activity: 'nothing' },
@@ -27,12 +29,21 @@ var icecreams = [
     { name: 'jawbreakers', photolink: 6, age: 2, activity: 'still nothing' },
 ];
 
+
 app.get("/", function(req, res) {
     res.render('index', { icecreams })
 });
 
 app.get("/home", function(req, res) {
     res.render('home', { icecreams })
+});
+
+app.get("/profiles", (req, res) => {
+    console.log("Yeee")
+    connection.query('SELECT * FROM guideinfo;', (err,data) => {
+        console.log(data);
+        res.render('profiles', {guideinfo: data});
+    })
 });
 
 app.listen(PORT, function() {
