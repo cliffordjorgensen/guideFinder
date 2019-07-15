7dbc158a7b0e35d00bdc8ecd56eddc04f7b29e89
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-07-15 16:12:29 -0700

    got rid of comments

--- config/orm.js
+++ config/orm.js
@@ -1,21 +1,14 @@
 // Import MySQL connection.
 var connection = require("./connection.js.js");
 
-// Helper function for SQL syntax.
-// Let's say we want to pass 3 values into the mySQL query.
-// In order to write the query, we need 3 question marks.
-// The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
     var arr = [];
 
     for (var i = 0; i < num; i++) {
         arr.push("?");
     }
-
     return arr.toString();
 }
-
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
     var arr = [];
