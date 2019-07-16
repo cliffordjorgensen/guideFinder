8f4bac13f4a488bfeabdb138680a5ca410b82466
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-07-15 19:11:28 -0700

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "password",
+        database: "guidefinder_db"
+    })
+}
+mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
