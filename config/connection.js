53c2375b56f8c0add7d1d5de433c248f31fddd14
cliffordjorgensen <cliffordjorgensen@gmail.com>
2019-07-15 19:24:40 -0700

    wokring on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,38 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "password",
-    database: "guidefinder_db"
-});
+var connection;
 
-// Make connection.
-connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "guidefinder_db"
+    });
+};
 
-// Export connection for our ORM to use.
+connection.connect();
 module.exports = connection;
+
+// mysql.createConnection({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "password",
+//     database: "guidefinder_db"
+// });
+
+// // Make connection.
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error("error connecting: " + err.stack);
+//         return;
+//     }
+//     console.log("connected as id " + connection.threadId);
+// });
+
+// // Export connection for our ORM to use.
+// module.exports = connection;
